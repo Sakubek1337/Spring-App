e0a8183f6b5a34c337f96918c759ae1dc419d7e7
Sakubek1337 <salamatbek10@gmail.com>
2022-04-09 18:46:21 +0600

    yes

---
+++ src/main/java/com/example/demo/repos/EmployeeRepo.java
@@ -0,0 +1,10 @@
+package com.example.demo.repos;
+
+import com.example.demo.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
+
+}
